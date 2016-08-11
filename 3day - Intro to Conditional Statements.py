6c05631b73155f5279944a14a671bd2387e320e0
Rafail <ahmetsha@gmail.com>
2016-08-11 19:16:40 +0300

    day 3 task

---
+++ 3day - Intro to Conditional Statements.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import sys
+
+
+N = int(input().strip())
+
+if N % 2 == 1:
+    print("Weird")
+else:
+    if 2 <= N <= 5:
+        print("Not Weird")
+    elif 6 <= N <= 20:
+        print("Weird")
+    elif 20 < N:
+        print("Not Weird")
